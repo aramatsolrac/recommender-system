0adefb51a944b8ac20705c30aa6b78d80e21b4e0
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-03-25 11:14:43 -0600

    Style header and add props

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,46 @@
-function Header() {
-  return <p>Test</p>;
+import "./Header.scss";
+import Button from "../Button/Button";
+function Header({
+  handleBack,
+  handleSkip,
+  progressValue,
+  progressMax,
+  formIndex,
+  formLength,
+}) {
+  return (
+    <div className="header">
+      <div className="header__buttons">
+        <Button handleBack={handleBack} className="header__buttons-back" />
+        <Button
+          handleSkip={handleSkip}
+          children={"Skip"}
+          className="header__buttons-skip"
+        />
+      </div>
+      <div>
+        <h1 className="header__title">
+          Help Us{" "}
+          <span className="header__title-highlight">
+            Tailor Your experience
+          </span>{" "}
+          and Store
+        </h1>
+      </div>
+      <div className="header__progress">
+        <progress
+          className="header__progress-bar"
+          value="20"
+          max="100"
+          //   value={progressValue} -> need to pass the value via props
+          //   max={progressMax} -> need to pass the value via props
+        ></progress>
+        <span className="header__progress-number">
+          {formIndex}/{formLength}
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
