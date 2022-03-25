c0137d51d2dc7be396bc8f64f7b282eee9429a71
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-03-25 09:38:55 -0600

    Update routers

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.scss";
 import { Route, Switch } from "react-router-dom";
 import Header from "./components/Header/Header.js";
+import RecommenderPage from "./pages/RecommenderPage";
 
 function App() {
   return (
     <div className="container">
       <Header />
       <Switch>
-        <Route path="/" exact component={""} />
-        <Route path="/" component={""} />
+        <Route path="/" exact component={RecommenderPage} />
       </Switch>
     </div>
   );
