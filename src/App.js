7a2df848a50c12ecfc3f4bec20cee212a41b1ab6
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-03-25 11:13:15 -0600

    Remove Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./App.scss";
 import { Route, Switch } from "react-router-dom";
-import Header from "./components/Header/Header.js";
 import RecommenderPage from "./pages/RecommenderPage";
 
 function App() {
   return (
     <div className="container">
-      <Header />
       <Switch>
         <Route path="/" exact component={RecommenderPage} />
       </Switch>
