3f2a316b9c6a0ac31250b3fd1c73bb686e51e7f4
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-03-25 11:15:49 -0600

    Add handleBack, handleSkip, and   handleNext functions
    Note: Not working properly.

--- src/pages/RecommenderPage.js
+++ src/pages/RecommenderPage.js
@@ -1,19 +1,30 @@
 import "./RecommenderPage.scss";
 import { Component } from "react";
 // import axios from "axios";
-import Button from "../components/Button/Button";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
 
 // const baseURL = process.env.REACT_APP_API_URL;
 
 class RecommenderPage extends Component {
+  handleBack = () => {
+    this.props.history.goBack(); // go to previous page
+  };
+
+  handleSkip = () => {
+    this.props.history.push("/"); // push to the  'https://stornename.myshopify.com/admin'
+  };
+
+  handleNext = () => {
+    // go to the next question
+  };
+
   render() {
     return (
-      <Button
-        children="Next"
-        className=""
-        type="button"
-        onClick={this.handleSubmit}
-      />
+      <>
+        <Header handleBack={this.handleBack} handleSkip={this.handleSkip} />
+        <Footer handleNext={this.handleNext} />
+      </>
     );
   }
 }
